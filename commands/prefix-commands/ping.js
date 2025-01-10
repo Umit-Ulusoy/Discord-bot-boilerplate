64668bcf7fdb5ac72b8514908a652426eafbfc13
Ümit ULUSOY <88715653+Umit-Ulusoy@users.noreply.github.com>
2025-01-10 15:25:59 +0300

    feat: Create a slash command named ping

--- commands/prefix-commands/ping.js
+++ commands/prefix-commands/ping.js
@@ -1,6 +1,6 @@
 export default {
     name: 'ping',
-    description: 'Ping komutu, Pong yazar.',
+    description: 'Ping command, replies with pong.',
     execute(client, message, args) {
       message.reply('Pong! 🏓');  
     },
